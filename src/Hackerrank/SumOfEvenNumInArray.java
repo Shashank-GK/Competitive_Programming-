4f7134666b5b4d9cc3f51c19b3fe69e57383a012
Shashank GK <89540267+Shashank-GK@users.noreply.github.com>
2024-08-31 10:18:08 +0530

    Commit

---
+++ src/Hackerrank/SumOfEvenNumInArray.java
@@ -0,0 +1,33 @@
+package Hackerrank;
+
+import java.util.Scanner;
+
+public class SumOfEvenNumInArray {
+
+    public static int sumOfEvenNUmb(int[] arr) {
+        int sum = 0;
+        if (arr.length == 0) {
+            System.out.println("Array is empty");
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                sum = sum + arr[i];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = sumOfEvenNUmb(arr);
+        System.out.println("Sum of even numbers in the array is: " + sum);
+    }
+}
