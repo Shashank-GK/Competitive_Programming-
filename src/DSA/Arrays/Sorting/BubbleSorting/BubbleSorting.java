2c3c36a3dfff894f3ae85f695e8602b0137149c3
Shashank GK <89540267+Shashank-GK@users.noreply.github.com>
2024-12-15 22:35:10 +0530

    commit

--- src/DSA/Arrays/Sorting/BubbleSorting/BubbleSorting.java
+++ src/DSA/Arrays/Sorting/BubbleSorting/BubbleSorting.java
@@ -3,6 +3,25 @@ package DSA.Arrays.Sorting.BubbleSorting;
 import java.util.Scanner;
 
 public class BubbleSorting {
+
+    public static int[] bubbleSorting(int[] arr) {
+        int n = arr.length;
+        // run the steps n-1 times
+        for (int i = 0; i < n; i++) {
+            // for each step, max item will come at the last respective index
+            for (int j = 0; j < n - 1; j++) {
+                // swap if the item is greater than the next item
+                int temp = 0;
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
@@ -13,4 +32,6 @@ public class BubbleSorting {
             arr[i] = sc.nextInt();
         }
     }
+
+
 }
