5f713941fb8e18e79daf77181fd3715458125bff
Shashank GK <89540267+Shashank-GK@users.noreply.github.com>
2024-12-19 11:30:36 +0530

    Commit

--- src/DSA/Arrays/Sorting/BubbleSorting/BubbleSorting.java
+++ src/DSA/Arrays/Sorting/BubbleSorting/BubbleSorting.java
@@ -27,7 +27,7 @@ public class BubbleSorting {
         }
         return arr;
     }
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
